7b6b5d4521b0b6a2ee6f29c8845962105af69bb9
Henrik Kurelid <henrik@kurelid.se>
2026-01-08 09:57:53 +0100

    Sub-issue (163): enable apu_mixer noise.nes (#165)

--- src/blargg_tests.rs
+++ src/blargg_tests.rs
@@ -485,4 +485,5 @@ mod tests {
     );
 
     blargg_test!(test_apu_mixer_dmc, "roms/blargg/apu_mixer/dmc.nes", 60 * 10);
+    blargg_test!(test_apu_mixer_noise, "roms/blargg/apu_mixer/noise.nes", 60 * 10);
 }
