40f8ab134f05c5b595c01f2e92992a252b301f87
Henrik Kurelid <henrik@kurelid.se>
2026-01-08 02:35:38 +0100

    Sub-issue (149): enable apu_test 5 and fix frame counter timing (#150)

--- src/blargg_tests.rs
+++ src/blargg_tests.rs
@@ -467,10 +467,10 @@ mod tests {
         test_apu_test_4,
         "roms/blargg/apu_test/rom_singles/4-jitter.nes"
     );
-    // blargg_test!(
-    //     test_apu_test_5,
-    //     "roms/blargg/apu_test/rom_singles/5-len_timing.nes"
-    // );
+    blargg_test!(
+        test_apu_test_5,
+        "roms/blargg/apu_test/rom_singles/5-len_timing.nes"
+    );
     // blargg_test!(
     //     test_apu_test_6,
     //     "roms/blargg/apu_test/rom_singles/6-irq_flag_timing.nes"
