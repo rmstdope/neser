439b7ee121c66dcc4f97b201e8163c65fedacd27
Henrik Kurelid <henrik@kurelid.se>
2026-01-08 10:29:14 +0100

    Refactor project to support optional SDL feature and update launch configuration

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@
 // This is not published or exposed externally
 
 pub mod apu;
-pub mod audio;
 pub mod blargg_tests;
 pub mod cartridge;
 pub mod cpu;
-pub mod eventloop;
 pub mod input;
 pub mod mem_controller;
 pub mod nes;
 pub mod ppu; // Modular PPU structure
 pub mod screen_buffer;
+
+#[cfg(feature = "sdl")]
+pub mod audio;
+
+#[cfg(feature = "sdl")]
+pub mod eventloop;
