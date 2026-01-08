782ee2726464a48427bc58086d9d6074a28c86f0
Henrik Kurelid <henrik@kurelid.se>
2026-01-08 11:37:02 +0100

    Comment out DMC test cases for buffer retention, latency, status, and IRQ handling

--- src/blargg_tests.rs
+++ src/blargg_tests.rs
@@ -525,24 +525,24 @@ mod tests {
         60 * 10
     );
 
-    blargg_test!(
-        test_dmc_tests_buffer_retained,
-        "roms/blargg/dmc_dma_during_read4/dma_2007_read.nes",
-        60 * 10
-    );
-    blargg_test!(
-        test_dmc_tests_latency,
-        "roms/blargg/dmc_tests/latency.nes",
-        60 * 10
-    );
-    blargg_test!(
-        test_dmc_tests_status,
-        "roms/blargg/dmc_tests/status.nes",
-        60 * 10
-    );
-    blargg_test!(
-        test_dmc_tests_status_irq,
-        "roms/blargg/dmc_tests/status_irq.nes",
-        60 * 10
-    );
+    // blargg_test!(
+    //     test_dmc_tests_buffer_retained,
+    //     "roms/blargg/dmc_tests/buffer_retained.nes",
+    //     60 * 10
+    // );
+    // blargg_test!(
+    //     test_dmc_tests_latency,
+    //     "roms/blargg/dmc_tests/latency.nes",
+    //     60 * 10
+    // );
+    // blargg_test!(
+    //     test_dmc_tests_status,
+    //     "roms/blargg/dmc_tests/status.nes",
+    //     60 * 10
+    // );
+    // blargg_test!(
+    //     test_dmc_tests_status_irq,
+    //     "roms/blargg/dmc_tests/status_irq.nes",
+    //     60 * 10
+    // );
 }
