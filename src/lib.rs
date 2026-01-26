e6226a34a63553eae431afe2579f0ef06ca3527c
Henrik Kurelid <henrik@kurelid.se>
2026-01-26 02:08:25 +0100

    Add WASM support with initial implementation and tests

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,10 @@ pub mod debugging;
 pub mod input;
 pub mod ppu;
 #[cfg(feature = "wasm")]
+#[path = "web_frontend/wasm.rs"]
 pub mod wasm;
 #[cfg(all(test, feature = "wasm", target_arch = "wasm32"))]
+#[path = "web_frontend/wasm_tests.rs"]
 mod wasm_tests;
 
 pub mod integration_tests;
