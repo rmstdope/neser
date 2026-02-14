79957bdc8dd26e52ef410853a8a5df7ea474acc0
Henrik Kurelid <henrik@kurelid.se>
2026-02-14 13:35:10 +0100

    feat(web): add fullscreen shortcut action parity
    Implements SDL-overlap web shortcuts for core actions in fullscreen via shared action dispatch (Space/F1/F6/F7/F12), with focused tests.
    
    Fixes #571

---
+++ web/shortcut_actions.js
@@ -0,0 +1,41 @@
+/**
+ * Dispatches a keyboard event to a supported web shortcut action.
+ *
+ * Returns true when a mapped shortcut was handled.
+ *
+ * @param {KeyboardEvent|{code:string,preventDefault:Function}} event
+ * @param {{
+ *   togglePause: Function,
+ *   reset: Function,
+ *   saveState: Function,
+ *   loadState: Function,
+ *   toggleFullscreen: Function
+ * }} actions
+ * @returns {Promise<boolean>}
+ */
+export async function dispatchWebShortcutAction(event, actions) {
+    const action = shortcutActionForCode(event.code, actions);
+    if (!action) {
+        return false;
+    }
+
+    event.preventDefault();
+    await action();
+    return true;
+}
+
+const shortcutActionByCode = {
+    Space: "togglePause",
+    F1: "reset",
+    F6: "saveState",
+    F7: "loadState",
+    F12: "toggleFullscreen"
+};
+
+function shortcutActionForCode(code, actions) {
+    const actionName = shortcutActionByCode[code];
+    if (!actionName) {
+        return null;
+    }
+    return actions[actionName];
+}
