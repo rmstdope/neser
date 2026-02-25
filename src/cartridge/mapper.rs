02903c99dc6a2596f65645ac665d9ede91240e14
Henrik Kurelid <henrik@kurelid.se>
2026-02-25 02:34:13 +0100

    feat(mapper): implement Mapper 244 (Decathlon) (#722)
    Implement iNES Mapper 244 with:
    - 32KB PRG banks selected via lookup table (identity)
    - 8KB CHR-ROM banks selected via scrambled lookup table
    - Copy-protection: CHR pairs (1,2) and (5,6) are swapped
    - Fixed mirroring from header
    
    Fixes #713

--- src/cartridge/mapper.rs
+++ src/cartridge/mapper.rs
@@ -30,6 +30,7 @@ use super::mapper64::Mapper64;
 use super::mapper65::Mapper65;
 use super::mapper241::Mapper241;
 use super::mapper242::Mapper242;
+use super::mapper244::Mapper244;
 use super::mmc1::MMC1Mapper;
 use super::mmc2::MMC2Mapper;
 use super::mmc3::MMC3Mapper;
@@ -746,6 +747,7 @@ mapper_registry! {
     206 => Namco118Mapper::new,
     241 => Mapper241::new,
     242 => Mapper242::new,
+    244 => Mapper244::new,
 }
 
 #[cfg(test)]
@@ -753,7 +755,7 @@ const SUPPORTED_MAPPERS: &[u8] = &[
     4, // MMC3 is constructed with CRC-specific behavior.
     0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 15, 16, 17, 19, 21, 22, 23, 24, 25, 26, 32, 33, 34, 40,
     42, 44, 45, 46, 47, 49, 50, 51, 52, 53, 56, 57, 58, 60, 61, 62, 64, 65, 66, 68, 69, 71, 78,
-    206, 241, 242,
+    206, 241, 242, 244,
 ];
 
 /// List of supported iNES mapper IDs handled by the factory.
