2cf0619e5769a9a825e41c8ae785066437eb82f3
Henrik Kurelid <henrik@kurelid.se>
2026-03-17 09:42:09 +0100

    feat(#1636): add extended console type variants to HardwareType
    Add VT01-VT369, UMC UM6578, and Famicom Network System variants.
    Update from_console_type_and_timing() to map Extended(n) to specific variants.
    Update from_db_value() to assign values 7-15 to extended variants.

--- src/cartridge/hardware_type.rs
+++ src/cartridge/hardware_type.rs
@@ -10,6 +10,15 @@ pub enum HardwareType {
     Dendy,
     Playchoice10,
     NesMultiRegion,
+    Vt01Monochrome,
+    Vt01Stn,
+    Vt02,
+    Vt03,
+    Vt09,
+    Vt32,
+    Vt369,
+    UmcUm6578,
+    FamicomNetworkSystem,
 }
 
 impl HardwareType {
@@ -23,6 +32,15 @@ impl HardwareType {
             4 => Some(Self::Dendy),
             5 => Some(Self::Playchoice10),
             6 => Some(Self::NesMultiRegion),
+            7 => Some(Self::Vt01Monochrome),
+            8 => Some(Self::Vt01Stn),
+            9 => Some(Self::Vt02),
+            10 => Some(Self::Vt03),
+            11 => Some(Self::Vt09),
+            12 => Some(Self::Vt32),
+            13 => Some(Self::Vt369),
+            14 => Some(Self::UmcUm6578),
+            15 => Some(Self::FamicomNetworkSystem),
             _ => None,
         }
     }
@@ -30,7 +48,19 @@ impl HardwareType {
     #[allow(dead_code)]
     pub fn timing_mode(self) -> TimingMode {
         match self {
-            Self::NesNtsc | Self::Famicom | Self::VsSystem | Self::Playchoice10 => TimingMode::Ntsc,
+            Self::NesNtsc
+            | Self::Famicom
+            | Self::VsSystem
+            | Self::Playchoice10
+            | Self::Vt01Monochrome
+            | Self::Vt01Stn
+            | Self::Vt02
+            | Self::Vt03
+            | Self::Vt09
+            | Self::Vt32
+            | Self::Vt369
+            | Self::UmcUm6578
+            | Self::FamicomNetworkSystem => TimingMode::Ntsc,
             Self::NesPal => TimingMode::Pal,
             Self::Dendy => TimingMode::Dendy,
             Self::NesMultiRegion => TimingMode::MultiRegion,
@@ -45,7 +75,19 @@ impl HardwareType {
         match console_type {
             ConsoleType::VsSystem => Self::VsSystem,
             ConsoleType::Playchoice10 => Self::Playchoice10,
-            ConsoleType::NesFamicom | ConsoleType::Extended(_) => match timing_mode {
+            ConsoleType::Extended(ext) => match ext {
+                3 => Self::Vt01Monochrome,
+                4 => Self::Vt01Stn,
+                5 => Self::Vt02,
+                6 => Self::Vt03,
+                7 => Self::Vt09,
+                8 => Self::Vt32,
+                9 => Self::Vt369,
+                10 => Self::UmcUm6578,
+                11 => Self::FamicomNetworkSystem,
+                _ => Self::NesNtsc,
+            },
+            ConsoleType::NesFamicom => match timing_mode {
                 TimingMode::Pal => Self::NesPal,
                 TimingMode::Dendy => Self::Dendy,
                 TimingMode::MultiRegion => Self::NesMultiRegion,
@@ -98,11 +140,6 @@ mod tests {
             HardwareType::from_db_value(6),
             Some(HardwareType::NesMultiRegion)
         );
-    }
-
-    #[test]
-    fn from_db_value_7_returns_none() {
-        assert_eq!(HardwareType::from_db_value(7), None);
     }
 
     #[test]
@@ -200,10 +237,47 @@ mod tests {
     }
 
     #[test]
-    fn from_console_extended_defaults_to_nes_ntsc() {
+    fn from_console_extended_3_returns_vt01_monochrome() {
+        assert_eq!(
+            HardwareType::from_console_type_and_timing(ConsoleType::Extended(3), TimingMode::Ntsc),
+            HardwareType::Vt01Monochrome
+        );
+    }
+
+    #[test]
+    fn from_console_extended_5_returns_vt02() {
         assert_eq!(
             HardwareType::from_console_type_and_timing(ConsoleType::Extended(5), TimingMode::Ntsc),
+            HardwareType::Vt02
+        );
+    }
+
+    #[test]
+    fn from_console_extended_unknown_defaults_to_nes_ntsc() {
+        assert_eq!(
+            HardwareType::from_console_type_and_timing(ConsoleType::Extended(99), TimingMode::Ntsc),
             HardwareType::NesNtsc
         );
+    }
+
+    #[test]
+    fn from_db_value_7_returns_vt01_monochrome() {
+        assert_eq!(
+            HardwareType::from_db_value(7),
+            Some(HardwareType::Vt01Monochrome)
+        );
+    }
+
+    #[test]
+    fn from_db_value_15_returns_famicom_network_system() {
+        assert_eq!(
+            HardwareType::from_db_value(15),
+            Some(HardwareType::FamicomNetworkSystem)
+        );
+    }
+
+    #[test]
+    fn from_db_value_16_returns_none() {
+        assert_eq!(HardwareType::from_db_value(16), None);
     }
 }
